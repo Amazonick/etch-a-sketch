83152f28ee02d4805395d512f068c161f39d9b25
Amazonick <pzs@live.com>
2022-03-30 15:24:10 +0200

    Made a 16x16 grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const mainContainer = document.querySelector("#container");
+
+function makeRows(rows, columns) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-columns', columns);
+    for (c = 0; c < (rows * columns); c++) {
+      let cell = document.createElement("div");
+      container.appendChild(cell).className = "grid-item";
+    };
+  };
+  
+  makeRows(16, 16);
